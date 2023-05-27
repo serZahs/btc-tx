3cedb2ca53176bfeebe6a07d7cdb1aaf517ef49b
serZahs <32044517+serZahs@users.noreply.github.com>
2023-05-27 05:02:37 -0400

    Validate transactions!

--- transactions.go
+++ transactions.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"bytes"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/sha256"
 	"encoding/binary"
 	"errors"
-	"fmt"
+	"math/big"
+
+	"github.com/dustinxie/ecc"
 )
 
 func DeserializeTransaction(raw_data []byte) (*Transaction, error) {
@@ -83,40 +88,146 @@ func DeserializeTransaction(raw_data []byte) (*Transaction, error) {
 	return &t, nil
 }
 
-func RunScript(script []byte) error {
-	//fmt.Printf("%x\n", script)
+func SerializeTransaction(t Transaction) ([]byte, error) {
+	buffer := bytes.NewBuffer(nil)
+
+	binary.Write(buffer, binary.LittleEndian, t.version)
+
+	if t.input_count > 0xfc {
+		return nil, errors.New("variable length sizes not supported yet")
+	}
+	binary.Write(buffer, binary.LittleEndian, byte(t.input_count))
+
+	for _, v := range t.inputs {
+		for i := 31; i >= 0; i-- {
+			binary.Write(buffer, binary.LittleEndian, v.transaction_hash[i])
+		}
+		binary.Write(buffer, binary.LittleEndian, v.output_index)
+
+		if v.script_size > 0xfc {
+			return nil, errors.New("variable length sizes not supported yet")
+		}
+		binary.Write(buffer, binary.LittleEndian, byte(v.script_size))
+		if v.script_size != 0x00 {
+			binary.Write(buffer, binary.LittleEndian, v.script)
+		}
+		binary.Write(buffer, binary.LittleEndian, v.sequence)
+	}
+
+	if t.output_count > 0xfc {
+		return nil, errors.New("variable length sizes not supported yet")
+	}
+	binary.Write(buffer, binary.LittleEndian, byte(t.output_count))
+
+	for _, v := range t.outputs {
+		binary.Write(buffer, binary.LittleEndian, v.amount)
+		if v.script_size > 0xfc {
+			return nil, errors.New("variable length sizes not supported yet")
+		}
+		binary.Write(buffer, binary.LittleEndian, byte(v.script_size))
+		if v.script_size != 0x00 {
+			binary.Write(buffer, binary.LittleEndian, v.script)
+		}
+	}
+
+	binary.Write(buffer, binary.LittleEndian, t.lock_time)
+
+	return buffer.Bytes(), nil
+}
+
+func ValidateTransactionScript(tx_new Transaction, tx_prev Transaction, input_index int) error {
+	output_index := tx_new.inputs[input_index].output_index
+	subscript := tx_prev.outputs[output_index].script
+
+	script := tx_new.inputs[input_index].script
+	script = append(script, subscript...)
+
 	var stack Stack
 	stack_init(&stack)
 	for i := 0; i < len(script); i++ {
 		switch b := script[i]; b {
-		case 0x76:
-			fmt.Println("OP_DUP")
+		case OP_DUP:
+			//fmt.Println("OP_DUP")
 			top := stack_peek(&stack)
 			stack_push(&stack, top)
-		case 0x88:
-			fmt.Println("OP_EQUALVERIFY")
+		case OP_EQUALVERIFY:
+			//fmt.Println("OP_EQUALVERIFY")
 			x1 := stack_pop(&stack)
 			x2 := stack_pop(&stack)
 			result := bytes.Equal(x1, x2)
 			if !result {
 				return errors.New("transaction validation failed")
 			}
-			stack_print(&stack)
-		case 0xa9:
-			fmt.Println("OP_HASH160")
+		case OP_HASH160:
+			//fmt.Println("OP_HASH160")
 			top := stack_pop(&stack)
 			stack_push(&stack, Hash160(top))
-		case 0xac:
-			fmt.Println("OP_CHECKSIG")
-			return nil
+		case OP_CHECKSIG:
+			//fmt.Println("OP_CHECKSIG")
+			public_key := stack_pop(&stack)
+			signature := stack_pop(&stack)
+
+			index := 3
+			r_length := signature[index]
+			index += 1
+			r_bytes := signature[index : index+int(r_length)]
+			index += int(r_length) + 1
+			s_length := signature[index]
+			index += 1
+			s_bytes := signature[index : index+int(s_length)]
+			index += int(s_length)
+			sig_hash := signature[index]
+
+			if sig_hash != 0x01 {
+				return errors.New("only SIGHASH_ALL is supported")
+			}
+
+			r := new(big.Int).SetBytes(r_bytes)
+			s := new(big.Int).SetBytes(s_bytes)
+
+			for i := range tx_new.inputs {
+				v := &tx_new.inputs[i]
+				if i == input_index {
+					v.script_size = compact_size(len(subscript))
+					v.script = subscript
+				} else {
+					v.script_size = 0x00
+					v.script = nil
+				}
+			}
+
+			serialized, err := SerializeTransaction(tx_new)
+			if err != nil {
+				return err
+			}
+			serialized = append(serialized, []byte{sig_hash, 0, 0, 0}...)
+
+			hashed := sha256.Sum256(serialized)
+			hashed = sha256.Sum256(hashed[:])
+
+			curve := ecc.P256k1()
+			x, y := elliptic.Unmarshal(curve, public_key)
+			pk := ecdsa.PublicKey{Curve: curve, X: x, Y: y}
+
+			if ecdsa.Verify(&pk, hashed[:], r, s) {
+				stack_push(&stack, []byte{1})
+			} else {
+				stack_push(&stack, []byte{0})
+			}
 		default:
 			if b >= 0x01 && b <= 0x4b {
-				fmt.Println("OP_PUSHDATA0")
+				//fmt.Println("OP_PUSHDATA0")
 				stack_push(&stack, script[i+1:i+int(b)+1])
 				i += int(b)
 			} else {
 				return errors.New("opcode not supported")
 			}
+		}
+	}
+	if stack.pos > 0 {
+		val := stack_pop(&stack)
+		if val[0] == 0 {
+			return errors.New("transaction validation failed")
 		}
 	}
 	return nil
